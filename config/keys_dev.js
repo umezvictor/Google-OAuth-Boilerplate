5b7b6e74633dc852be9005143cf3786da9343e7e
Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2019-09-20 16:15:36 +0100

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MONGO_URI: 'mongodb://localhost:27017/devconnectdb',
-    SengridApiKey: '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',
-    JWT_SECRET: 'victorblaze1900',
-    clientID: '391231827120-b6sqekb1i4b16rmtapekobsbvlm0f7sf.apps.googleusercontent.com',
-    clientSecret: 'vh2pzRxuGNGhMXUDLICvWYiF',
-    cookieKey: 'mycookiekey'
-};
